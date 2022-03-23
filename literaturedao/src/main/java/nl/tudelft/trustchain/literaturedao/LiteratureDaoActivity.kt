bb210c982b36d9ff122f6a11613ab44d90ace70e
Marko Matusovic <m.matusovic@student.tudelft.nl>
2022-03-23 15:19:21 +0100

    Add saving and reading files, and listing directory of files

--- literaturedao/src/main/java/nl/tudelft/trustchain/literaturedao/LiteratureDaoActivity.kt
+++ literaturedao/src/main/java/nl/tudelft/trustchain/literaturedao/LiteratureDaoActivity.kt
@@ -2,9 +2,7 @@ package nl.tudelft.trustchain.literaturedao
 import nl.tudelft.trustchain.common.BaseActivity
 
 import android.util.Log
-import com.tom_roush.pdfbox.android.PDFBoxResourceLoader
-import nl.tudelft.trustchain.literaturedao.controllers.KeywordExtractor
-import nl.tudelft.trustchain.literaturedao.controllers.PdfController
+import nl.tudelft.trustchain.literaturedao.controllers.FileStorageController
 import java.io.InputStream
 
 open class LiteratureDaoActivity : BaseActivity() {
@@ -13,42 +11,14 @@ open class LiteratureDaoActivity : BaseActivity() {
 
     override fun onStart() {
         super.onStart()
-        Log.e("litdao", "starting ...")
-        PDFBoxResourceLoader.init(getApplicationContext());
-        var pdfController = PdfController()
-        var i = 1
-        while (i < 2){
-            val stream: InputStream = getAssets().open(i.toString() + ".pdf")
-            //val csv: InputStream = getAssets().open("stemmed_freqs.csv")
-            //val result: String
-            val result = KeywordExtractor()
-                .quikFix(pdfController
-                    .stripText(stream))
-                .toString()
-            /*
-            if (csv != null){
-                val reader = BufferedReader(InputStreamReader(csv))
-                val result = KeywordExtractor()
-                    .actualImplementation(pdfController
-                        .stripText(stream), reader)
-                    .toString()
-            } else {
-                val result = KeywordExtractor()
-                    .quikFix(pdfController
-                        .stripText(stream))
-                    .toString()
-            }*/
+        Log.d("litdao", "starting ...")
 
-            Log.e("litdao", "litdao: " + result)
-            i += 1
-        }
-        //Log.d("litdao", pdfController.stripText(stream))
+        Log.d("litdao", applicationContext.cacheDir.absolutePath)
 
-/*
-        Snackbar sb = Snackbar.make(findViewById(R.id.linearLayout), R.string.offline_message, Snackbar.LENGTH);
-        snackbar.show
-        ()
-*/
-        //Toast.makeText(LiteratureDaoActivity(), result, Toast.LENGTH_SHORT).show();
+        // This is how you initialise the FileStorageController
+        // the applicationCOntext needs to be passed from a place where it's accessible, such as here
+        val docController = FileStorageController(applicationContext.cacheDir.absolutePath)
+
+        Log.d("litdao", docController.listFiles().size.toString())
     }
 }
